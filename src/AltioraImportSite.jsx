60895021e6528120c421982a927fb166703d8b43
KHALDI-99 <95024628+KHALDI-99@users.noreply.github.com>
2026-03-14 13:38:34 +0100

    Add files via upload

---
+++ src/AltioraImportSite.jsx
@@ -0,0 +1,11 @@
+
+// Place here the AltioraImportSite.jsx component generated previously
+// (copy/paste the file if you already downloaded it)
+export default function AltioraImportSite(){
+return (
+<div style={{fontFamily:"sans-serif",padding:40}}>
+<h1>Altiora International Limited Company</h1>
+<p>Import & sourcing from China</p>
+</div>
+)
+}
---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+
+import AltioraImportSite from "./AltioraImportSite"
+
+export default function App(){
+  return <AltioraImportSite/>
+}
